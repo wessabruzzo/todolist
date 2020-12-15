5625f2c15048564b992ec863d26d0dadc5c9f8ee
Wess A <Wess A>
2020-12-15 07:14:42 -0800

    Added html css, script, h1, form, grid

---
+++ script.js
@@ -0,0 +1,31 @@
+//To do list form
+document.forms.myForm;
+var myForm = document.forms.myForm;
+document.getElementById("my-form").addEventListener("submit", button);
+//Form validation
+function button(){
+
+
+};
+
+
+
+
+/*
+    if(myForm.name.value == ""){
+        message.innerText = "LET'S GO!!! Enter something!";
+        return false;
+    } else{
+        message.innerText = "";
+        return true;
+    }
+
+
+const addFrom = document.forms['my-form'];
+
+addFrom.addEventListener('submit', function(e){
+    e.preventDefault();
+    const value = addForm.querySelector('input[type="text"]').value;
+    console.log(value);
+});
+*/
